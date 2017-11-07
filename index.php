898c74ef4fc6a074f71ae18e40c1fb7baa963d0d
YanaGumbatova555 <31986459+YanaGumbatova555@users.noreply.github.com>
2017-11-07 10:06:29 +0300

    Update index.php

--- index.php
+++ index.php
@@ -31,6 +31,8 @@ enctype="multipart/form-data" >
 name="name" id="name"/></br>
       Email <input type="text" 
 name="email" id="email"/></br>
+     Password <input type="text" 
+name="password" id="password"/></br>
       <input type="submit" 
 name="submit" value="Отправить" />
 </form>
