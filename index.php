4ada9382d83bc1da65aa956f2dc1c41867bc439d
YanaGumbatova555 <31986459+YanaGumbatova555@users.noreply.github.com>
2018-03-13 22:48:32 +0300

    Create index.php

---
+++ index.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <link rel="stylesheet" href="/css/style.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
+    <title>Авторизация</title>
+    <style>
+    .container {
+      width: 300px;
+      height: 320px;
+      margin: 200px auto 0 auto;
+      text-align: center;
+      background-color: #afacaa;
+    }
+    .sub {
+      text-align: center;
+      margin-left: 90px;
+      width: 95px;
+      height: 30px;
+    }
+    .form-control {
+      width: 270px;
+    }
+    </style>
+  </head>
+  <body>
+    <div class="container">
+      <div class="row">
+      	<div class="col-sm-4">
+          <form class="" action="index.html" method="post">
+            <legend><h1>Авторизация</h1></legend>
+            <div class="form-group">
+              <label for="">Логин</label>
+              <input class="form-control" type="text" name="username" value="">
+            </div>
+            <div class="form-group">
+              <label for="">Пароль</label>
+              <input class="form-control" type="password" name="password" value="">
+            </div>
+            <input class="sub" type="submit" name="submit" value="Войти">
+            <br><br>
+            <a href="\sugnup.html" style="font-size: 14px; margin-left: 100px;">Регистрация</a>
+          </div>
+        </div>
+      </form>
+    </div>
+  </body>
+</html>
