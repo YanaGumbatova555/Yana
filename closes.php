1f3dd961ffc3aba685c7e5394b69a6769851b491
YanaGumbatova555 <31986459+YanaGumbatova555@users.noreply.github.com>
2018-03-16 12:38:02 +0300

    Update closes.php

--- closes.php
+++ closes.php
@@ -4,21 +4,21 @@
       <div class="s_listing s_grid_view clearfix">
         <div class="s_item grid_2"><img src="images/dummy/pic_5.jpg" title="Armani Acqua di Gioia" alt="Armani Acqua di Gioia" /></a>
           <h3><a href="product.html">Armani Acqua di Gioia</a></h3>
-          <p class="s_model">Product 1</p>
+          <p class="s_model">Товар №1</p>
           <p class="s_price s_promo_price"><span class="s_old_price"><span class="s_currency s_before">$</span>100</span><span class="s_currency s_before">$</span>80</p>
-          <a class="s_button_add_to_cart" href="product.html"><span class="s_icon_16"><span class="s_icon"></span>Add to Cart</span></a>
+          <a class="s_button_add_to_cart" href="product.html"><span class="s_icon_16"><span class="s_icon"></span>Добавить в корзину</span></a>
         </div>
         <div class="s_item grid_2"> <a class="s_thumb" href="product.html"><img src="images/dummy/pic_6.jpg" title="Hennesy Paradis Extra" alt="Hennesy Paradis Extra" /></a>
           <h3><a href="product.html">Hennesy Paradis Extra</a></h3>
-          <p class="s_model">Product 2</p>
+          <p class="s_model">Товар №2</p>
           <p class="s_price s_promo_price"><span class="s_old_price"><span class="s_currency s_before">$</span>300</span><span class="s_currency s_before">$</span>250</p>
-          <a class="s_button_add_to_cart" href="product.html"><span class="s_icon_16"><span class="s_icon"></span>Add to Cart</span></a>
+          <a class="s_button_add_to_cart" href="product.html"><span class="s_icon_16"><span class="s_icon"></span>Добавить в корзину</span></a>
         </div>
         <div class="s_item grid_2"> <a class="s_thumb" href="product.html"><img src="images/dummy/pic_7.jpg" title="Leica M7" alt="Leica M7" /></a>
           <h3><a href="product.html">Leica M7</a></h3>
-          <p class="s_model">Product 3</p>
+          <p class="s_model">Товар №3</p>
           <p class="s_price s_promo_price"><span class="s_old_price"><span class="s_currency s_before">$</span>150</span><span class="s_currency s_before">$</span>100</p>
-          <a class="s_button_add_to_cart" href="product.html"><span class="s_icon_16"><span class="s_icon"></span>Add to Cart</span></a>
+          <a class="s_button_add_to_cart" href="product.html"><span class="s_icon_16"><span class="s_icon"></span>Добавить в корзину</span></a>
         </div>
        
         <div class="clear"></div>
