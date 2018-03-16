0b9bee80cb2607d323e595f9a4c06607fcfea61b
YanaGumbatova555 <31986459+YanaGumbatova555@users.noreply.github.com>
2018-03-16 11:45:11 +0300

    Create cart.php

---
+++ cart.php
@@ -0,0 +1,12 @@
+class Application_Models_Cart
+  {  
+    function addToCart($id, $count=1)
+    {        
+     $_SESSION['cart'][$id]=$_SESSION['cart'][$id]+$count;     
+    return true;
+    }    
+     
+    function delFromCart($id, $count=1){}
+     
+    function clearCart(){}
+  }
