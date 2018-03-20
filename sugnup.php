187cd6ab375b33873880a9d88a3e0352432e91e0
YanaGumbatova555 <31986459+YanaGumbatova555@users.noreply.github.com>
2018-03-20 09:56:28 +0300

    Update sugnup.php

--- sugnup.php
+++ sugnup.php
@@ -30,7 +30,7 @@
     <div class="container">
       <div class="row">
         <div class="col-sm-4">
-            <form method="post" action="registration.php">
+            <form method="post" action="sugnup.php">
               <legend><h1>Регистрация</h1></legend>
               <div class="form-group">
                 <label for="" >Фамилия</label>
@@ -64,63 +64,3 @@
   </body>
 </html>
 
-<?php
-try 
-{
-  $conn = new PDO("sqlsrv:server = tcp:servgumb.database.windows.net,1433; Database = db1", "Yana", "Sobachka.1");
-  $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-}
-catch (PDOException $e) 
-{
-print("Error connecting to SQL Server.");
-die(print_r($e));
-}
-
-if(isset($_POST["submit"])) {
-if($_POST["lastname"] =="" || $_POST["name"]=="" || $_POST["middlename"] ==""){echo "Введите свои данные";}
-else{
-try {
-$lastname = $_POST['lastname'];
-$name = $_POST['name'];
-$middlename = $_POST['middlename'];
-$Email = $_POST['Email'];
-
-
-// Insert data 
-$sql_insert = "INSERT INTO client_tbl (lastname,name,middlename,Email ) VALUES (?,?,?,?)";
-$stmt = $conn->prepare($sql_insert);
-$stmt->bindValue(1, $lastname);
-$stmt->bindValue(2, $name);
-$stmt->bindValue(3, $middlename);
-$stmt->bindValue(4, $Email );
-$stmt->execute();
-}
-catch(Exception $e) 
-{
-die(var_dump($e));
-}
-}
-}
-$sql_select = "SELECT * FROM client_tbl";
-$stmt = $conn->query($sql_select);
-$registrants = $stmt->fetchAll();
-if(count($registrants) > 0) {
-echo "<table>";
-echo "<tr><th>lastname</th>";
-echo "<th>name</th>";
-echo "<th>middlename</th>";
-echo "<th>Email </th>";
-foreach($registrants as $registrant) {
-echo "<tr><td>".$registrant['lastname']."</td>";
-echo "<td>".$registrant['name']."</td>";
-echo "<td>".$registrant['middlename']."</td>";
-echo "<td>".$registrant['Email ']."</td>";
-}
-echo "</table>";
-}
-else {
-echo "<h3>Ни один пользователь не зарегистрирован.</h3>";
-}
-
-
-?>
