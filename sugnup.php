2c2e9b4096207527d23545cab5f96c9d48da97c8
YanaGumbatova555 <31986459+YanaGumbatova555@users.noreply.github.com>
2018-03-20 12:10:20 +0300

    Update sugnup.php

--- sugnup.php
+++ sugnup.php
@@ -120,10 +120,10 @@ $registrants = $stmt->fetchAll();
 if(count($registrants) > 0) 
 {
 echo "<table>";
-echo "<tr><th>lastname     </th>";
-echo "<th>name     </th>";
-echo "<th>middlename    </th>";
-echo "<th>Email    </th></tr>";
+echo "<tr><th>lastname     </th></tr>";
+echo "<tr><th>name     </th></tr>";
+echo "<tr><th>middlename    </th></tr>";
+echo "<tr><th>Email    </th></tr>";
 }
 foreach($registrants as $registrant) 
 {
