93e5e4b604f9b771359f4b414204a6fc565d5829
YanaGumbatova555 <31986459+YanaGumbatova555@users.noreply.github.com>
2018-03-31 15:33:05 +0300

    Update leftblock.php

--- leftblock.php
+++ leftblock.php
@@ -1,9 +1,9 @@
 <div class="leftblock"> 
 <ul>
   <li><a href="\closes.php"> АКЦИЯ</a></li>
-  <li><a href="\closes.php"> Женская одежда</a></li>
-  <li><a href="\closes.php"> Мужская одежда</a></li>
-  <li><a href="\closes.php"> Детская одежда</a></li>
+  <li><a href="\closes1.php"> Женская одежда</a></li>
+  <li><a href="\closes2.php"> Мужская одежда</a></li>
+  <li><a href="\closes3.php"> Детская одежда</a></li>
 </ul>
 </div>
 
