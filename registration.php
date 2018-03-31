36ec820b16ff8b20f7f8e6e527b300475299a5ba
YanaGumbatova555 <31986459+YanaGumbatova555@users.noreply.github.com>
2018-03-31 23:38:59 +0300

    Update registration.php

--- registration.php
+++ registration.php
@@ -1,1 +1,15 @@
-registration.php
+<html>
+<head>
+<title>Интернет-магазин</title>
+<?php include_once "header.php" ?>
+  <?php include_once "leftblock.php" ?>
+  
+  <div class="content">
+  
+    <form action="query-registration.php">
+    </form>
+ 
+  </div>
+    
+  <?php include_once "footer.php" ?>
+</html>
