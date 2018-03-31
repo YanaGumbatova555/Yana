e7fd7b0d8d8c8b82cbd566c28f9c7803bb94c749
YanaGumbatova555 <31986459+YanaGumbatova555@users.noreply.github.com>
2018-03-31 13:27:12 +0300

    Update leftblock.php

--- leftblock.php
+++ leftblock.php
@@ -1,6 +1,9 @@
 <div class="leftblock"> 
 <ul>
-  <li><a href="https://github.com/YanaGumbatova555/Yana/blob/master/closes.php"</a></li>
+  <li><a href="https://github.com/YanaGumbatova555/Yana/blob/master/closes.php"> Товары по акции</a></li>
+   <li><a href="https://github.com/YanaGumbatova555/Yana/blob/master/closes.php"> Женская обувь</a></li>
+   <li><a href="https://github.com/YanaGumbatova555/Yana/blob/master/closes.php"> Мужская обувь</a></li>
+   <li><a href="https://github.com/YanaGumbatova555/Yana/blob/master/closes.php"> Детская обувь</a></li>
 </ul>
 </div>
 
