2dfa7d0e18c0f83ca4d6739882357ec9706d1949
YanaGumbatova555 <31986459+YanaGumbatova555@users.noreply.github.com>
2018-03-31 11:39:36 +0300

    Update index.php

--- index.php
+++ index.php
@@ -1,9 +1,9 @@
-
-    <!DOCTYPE html>
 <html>
-  <head>
-    <meta charset="utf-8">
-    <link rel="stylesheet" href="/css/style.css">
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
-    <title>Интернет-магазин</title>
-</html>
+
+<head>
+
+<Title>Интернет-магазин</Title>
+
+    </head>
+    
+    </html>
