82d1932a905175622eca278622619e62b19b51c6
YanaGumbatova555 <31986459+YanaGumbatova555@users.noreply.github.com>
2018-04-01 00:17:12 +0300

    Update connection.php

--- connection.php
+++ connection.php
@@ -1,3 +1,72 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
+    <title>Регистрация</title>
+    <style>
+    .container {
+      width: 300px;
+      height: 620px;
+      margin: 100px auto 0 auto;
+      text-align: center;
+      background-color: #afacaa;
+    }
+    .sub {
+      text-align: center;
+      margin-left: 90px;
+      width: 95px;
+      height: 30px;
+    }
+    .form-control {
+      width: 270px;
+    }
+    label {
+      float: left;
+    }
+    </style>
+  </head>
+  <body>
+    <div class="container">
+      <div class="row">
+        <div class="col-sm-4">
+            <form method="post" action="sugnup.php">
+              <legend><h1>Регистрация</h1></legend>
+              <div class="form-group">
+                <label for="" >Фамилия</label>
+                <input class="form-control" type="text" name="lastname" value="">
+              </div>
+              <div class="form-group">
+                <label for="">Имя</label>
+                <input class="form-control" type="text" name="name" value="">
+              </div>
+              <div class="form-group">
+                <label for="">Отчество</label>
+                <input class="form-control" type="text" name="middlename">
+              </div>
+              <div class="form-group">
+                <label for="">Email</label>
+                <input class="form-control" type="text" name="Email">
+              </div>
+              <div class="form-group">
+                <label for="">Логин</label>
+                <input class="form-control" type="text" name="login">
+              </div>
+              <div class="form-group">
+                <label for="">Пароль</label>
+                <input class="form-control" type="password" name="password">
+              </div>
+              <div class="form-group">
+                <label>Повторите пароль</label>
+                <input class="form-control" type="password" name="password2">
+              </div>
+              <input class="sub" type="submit" name="submit" value="Регистрация">
+            </form>
+        </div>
+      </div>
+    </div>
+  </body>
+</html>
 
 <?php
 try 
@@ -10,6 +79,7 @@ catch (PDOException $e)
 print("Error connecting to SQL Server.");
 die(print_r($e));
 }
+
 if (isset($_POST['submit'])) {
 $lastname = $_POST['lastname'];
 $name = $_POST['name'];
@@ -41,6 +111,7 @@ $err[] = 'Поле пароль не заполненно!';
 elseif($password != $password2) {
 $err[] = 'Неправельно заполнен пароль!';
 }
+
 $sql_insert = "INSERT INTO table1 (lastname, name, middlename, Email, login, password, password2) VALUES (?,?,?,?,?,?,?)";
 $stmt = $conn->prepare($sql_insert);
 $stmt->bindValue(1, $lastname);
@@ -52,6 +123,7 @@ $stmt->bindValue(6, $password);
 $stmt->bindValue(7, $password2);
 $stmt->execute();
 echo '<div style= "color: blue; text-align: center;">Вы зарегистрированны!</div><hr>';
+
 $sql_select = "SELECT * FROM table1";
 $stmt = $conn->query($sql_select);
 $registrants = $stmt->fetchAll();
@@ -74,5 +146,4 @@ echo "<td>".$registrant['Email']."</td>";
 }
 echo "</table>";
 }
-
-?>
+?> 
