dc3e225f0b9e0cbe47665538d1316fa64e27932e
YanaGumbatova555 <31986459+YanaGumbatova555@users.noreply.github.com>
2018-03-31 11:53:07 +0300

    Update index.php

--- index.php
+++ index.php
@@ -1,9 +1,35 @@
 <html>
-
 <head>
-
-<Title>Интернет-магазин</Title>
-
-    </head>
-    
-    </html>
+<title>Интернет-магазин</title>
+<?php
+require_once $_SERVER['DOCUMENT_ROOT'].'/head.php';
+?>
+</head>
+<body>
+<div class="shapka">
+<?php
+require_once $_SERVER['DOCUMENT_ROOT'].'/top.php';
+?>
+</div>
+<div class="page">
+<div class="sidebar_left">
+<?php
+require_once $_SERVER['DOCUMENT_ROOT'].'/sidebar1.php';
+?>
+</div>
+<div class="content">
+Основная часть
+</div>
+<div class="sidebar_right">
+<?php
+require_once $_SERVER['DOCUMENT_ROOT'].'/sidebar2.php';
+?>
+</div>
+<div class="footer">
+<?php
+require_once $_SERVER['DOCUMENT_ROOT'].'/footer.php';
+?>
+</div>
+</div>
+</body>
+</html>
