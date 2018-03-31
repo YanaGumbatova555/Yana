d62889c4dfc451243c21b0135403aeec77602c73
YanaGumbatova555 <31986459+YanaGumbatova555@users.noreply.github.com>
2018-03-31 22:47:45 +0300

    Update closes1.php

--- closes1.php
+++ closes1.php
@@ -3,7 +3,7 @@
 <tbody> 
 <tr><td style="border-image: initial; width="40" height="" align="center"> 
 <p align="center"><img src="closes/womencloses1.jpg" border="0" width="400" height="300"/></p>
-<h3><a href="product.html">Armani Acqua di Gioia</a></h3>
+<h3><a href="product.html">Майка</a></h3>
 <p class="s_model">Товар №1</p>
 <p class="s_price s_promo_price"><span class="s_old_price">
 <span class="s_currency s_before">$</span><strike>100</strike></span><span class="s_currency s_before">$</span>80</p>
@@ -11,7 +11,7 @@
       
 </td><td style="border-image: initial;" width="40" height="" align="center"></td><td style="border-image: initial;" align="center"> 
 <p align="center"><img src="closes/womencloses2.jpg.jpg" border="0" width="400" height="300"/></p>
-<h3><a href="product.html">Туфли Gucci</a></h3>
+<h3><a href="product.html">Джинсы</a></h3>
 <p class="s_model">Товар №2</p>
 <p class="s_price s_promo_price"><span class="s_old_price">
 <span class="s_currency s_before">$</span><strike>250</strike></span><span class="s_currency s_before">$</span>220</p>
@@ -21,7 +21,7 @@
       
 <tr align="center"><td style="border-image: initial;"> 
 <p align="center"><img src="closes/womencloses3.jpg.jpg" border="0" width="400" height="300"/></p>
-<h3><a href="product.html">Leica M7</a></h3>
+<h3><a href="product.html">Футболка</a></h3>
 <p class="s_model">Товар №3</p>
 <p class="s_price s_promo_price"><span class="s_old_price">
 <span class="s_currency s_before">$</span><strike>150</strike></span><span class="s_currency s_before">$</span>110</p>
@@ -29,7 +29,7 @@
       
 </td><td style="border-image: initial;" width="40" height=""></td><td style="border-image: initial;"> 
 <p align="center"><img src="closes/womencloses4.jpg.jpg" border="0" width="400" height="300"/></p>
-<h3><a href="product.html">Giorgio Armani Acqua di Gio pour homme</a></h3>
+<h3><a href="product.html">Брюки</a></h3>
 <p class="s_model">Товар №4</p>
 <p class="s_price s_promo_price"><span class="s_old_price">
 <span class="s_currency s_before">$</span><strike>110</strike></span><span class="s_currency s_before">$</span>90</p>
