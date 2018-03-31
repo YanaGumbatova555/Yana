ed5eecb97a7bce6411865c95f4d5bd4ceb8ad5ee
YanaGumbatova555 <31986459+YanaGumbatova555@users.noreply.github.com>
2018-04-01 00:43:20 +0300

    Update registration.php

--- registration.php
+++ registration.php
@@ -7,77 +7,16 @@
   <div class="content">
   
     <form action="query-registration.php">
-      <!DOCTYPE html>
-<html>
-  <head>
-    <meta charset="utf-8">
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
-    <title>Регистрация</title>
-    <style>
-    .container {
-      width: 300px;
-      height: 620px;
-      margin: 100px auto 0 auto;
-      text-align: center;
-      background-color: #afacaa;
-    }
-    .sub {
-      text-align: center;
-      margin-left: 90px;
-      width: 95px;
-      height: 30px;
-    }
-    .form-control {
-      width: 270px;
-    }
-    label {
-      float: left;
-    }
-    </style>
-  </head>
-  <body>
-    <div class="container">
-      <div class="row">
-        <div class="col-sm-4">
-            <form method="post" action="sugnup.php">
-              <legend><h1>Регистрация</h1></legend>
-              <div class="form-group">
-                <label for="" >Фамилия</label>
-                <input class="form-control" type="text" name="lastname" value="">
-              </div>
-              <div class="form-group">
-                <label for="">Имя</label>
-                <input class="form-control" type="text" name="name" value="">
-              </div>
-              <div class="form-group">
-                <label for="">Отчество</label>
-                <input class="form-control" type="text" name="middlename">
-              </div>
-              <div class="form-group">
-                <label for="">Email</label>
-                <input class="form-control" type="text" name="Email">
-              </div>
-              <div class="form-group">
-                <label for="">Логин</label>
-                <input class="form-control" type="text" name="login">
-              </div>
-              <div class="form-group">
-                <label for="">Пароль</label>
-                <input class="form-control" type="password" name="password">
-              </div>
-              <div class="form-group">
-                <label>Повторите пароль</label>
-                <input class="form-control" type="password" name="password2">
-              </div>
-              <input class="sub" type="submit" name="submit" value="Регистрация">
-            </form>
-        </div>
-      </div>
-    </div>
-  </body>
-</html>
+      <label>Логин:</label><br>
+      <input type="text" name="login"><br>
+      <label>Пароль:</label><br>
+      <input type="password" name="password"><br>
+      <label>Имя:</label><br>
+      <input type="text" name="name"><br>
+      <label>Фамилия:</label><br>
+      <input type="text" name="surname"><br>
+      <input type="submit" name="submit">
     </form>
- 
   </div>
     
   <?php include_once "footer.php" ?>
