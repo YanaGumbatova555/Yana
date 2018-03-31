438a76642c0a97d09540259e5d824567129b046b
YanaGumbatova555 <31986459+YanaGumbatova555@users.noreply.github.com>
2018-03-31 14:20:43 +0300

    Update closes.php

--- closes.php
+++ closes.php
@@ -1,40 +1,40 @@
 <div id="special_home" class="grid_12">
       <p style="text-align: center;"><b><font size="4" color="#9d0a0f"><font face="Verdana">Товары по акции</font> </font></b> </p>
-      <table width="996" height="711" cellspacing="1" cellpadding="1" border="0" align="center"> 
+<table width="996" height="711" cellspacing="1" cellpadding="1" border="0" align="center"> 
 <tbody> 
-<tr><td style="border-image: initial;"> 
-<p align="center"><img src="images/dummy/pic_5.jpg" border="0" width="400" height="300" title="Armani Acqua di Gioia" alt="Armani Acqua di Gioia" /></p>
+<tr><td style="border-image: initial; "> 
+<p align="center"><img src="images/dummy/pic_5.jpg" border="0" width="400" height="300"/></p>
 <h3><a href="product.html">Armani Acqua di Gioia</a></h3>
 <p class="s_model">Товар №1</p>
 <p class="s_price s_promo_price"><span class="s_old_price">
 <span class="s_currency s_before">$</span><strike>100</strike></span><span class="s_currency s_before">$</span>80</p>
-<a class="" href=".html"><span class="s_icon_16"><span class="s_icon"></span>Добавить в корзину</span></a>
-          
+<a class="" href=".html">Добавить в корзину</a>
+      
 </td><td style="border-image: initial;" width="40" height="" align="center"></td><td style="border-image: initial;" align="center"> 
-<p align="center"><img src="images/dummy/pic_6.jpg" border="0" width="400" height="300" title="Armani Acqua di Gioia" alt="Armani Acqua di Gioia" /></p>
-<h3><a href="product.html">Armani Acqua di Gioia</a></h3>
-<p class="s_model">Товар №1</p>
+<p align="center"><img src="images/dummy/pic_6.jpg" border="0" width="400" height="300"/></p>
+<h3><a href="product.html">Туфли Gucci</a></h3>
+<p class="s_model">Товар №2</p>
 <p class="s_price s_promo_price"><span class="s_old_price">
 <span class="s_currency s_before">$</span><strike>100</strike></span><span class="s_currency s_before">$</span>80</p>
-<a class="" href=".html"><span class="s_icon_16"><span class="s_icon"></span>Добавить в корзину</span></a>
+<a class="" href=".html">Добавить в корзину</a>
       
 </td></tr>
       
 <tr align="center"><td style="border-image: initial;"> 
-<p align="center"><img src="images/dummy/pic_7.jpg" border="0" width="400" height="300" title="Armani Acqua di Gioia" alt="Armani Acqua di Gioia" /></p>
-<h3><a href="product.html">Armani Acqua di Gioia</a></h3>
-<p class="s_model">Товар №1</p>
+<p align="center"><img src="images/dummy/pic_7.jpg" border="0" width="400" height="300"/></p>
+<h3><a href="product.html">Hennesy Paradis Extra</a></h3>
+<p class="s_model">Товар №3</p>
 <p class="s_price s_promo_price"><span class="s_old_price">
 <span class="s_currency s_before">$</span><strike>100</strike></span><span class="s_currency s_before">$</span>80</p>
- <a class="" href=".html"><span class="s_icon_16"><span class="s_icon"></span>Добавить в корзину</span></a>
+<a class="" href=".html">Добавить в корзину</a>
       
 </td><td style="border-image: initial;" width="40" height=""></td><td style="border-image: initial;"> 
-<p align="center"><img src="images/dummy/pic_8.jpg" border="0" width="400" height="300" title="Armani Acqua di Gioia" alt="Armani Acqua di Gioia" /></p>
-<h3><a href="product.html">Armani Acqua di Gioia</a></h3>
-<p class="s_model">Товар №1</p>
+<p align="center"><img src="images/dummy/pic_9.jpg" border="0" width="400" height="300"/></p>
+<h3><a href="product.html">Компактный фотоаппарат Panasonic Lumix DMC-FZ1000</a></h3>
+<p class="s_model">Товар №4</p>
 <p class="s_price s_promo_price"><span class="s_old_price">
 <span class="s_currency s_before">$</span><strike>100</strike></span><span class="s_currency s_before">$</span>80</p>
-<a class="" href=".html"><span class="s_icon_16"><span class="s_icon"></span>Добавить в корзину</span></a>
+<a class="" href=".html">Добавить в корзину</a>
 </td></tr>
 </tbody>
 </table>
