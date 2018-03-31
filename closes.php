e7be0190b80e4e9f4dbc1521dd356a06aae541f3
YanaGumbatova555 <31986459+YanaGumbatova555@users.noreply.github.com>
2018-03-31 14:24:49 +0300

    Update closes.php

--- closes.php
+++ closes.php
@@ -1,4 +1,4 @@
-<div id="special_home" class="grid_12">
+
       <p style="text-align: center;"><b><font size="4" color="#9d0a0f"><font face="Verdana">Товары по акции</font> </font></b> </p>
 <table width="996" height="711" cellspacing="1" cellpadding="1" border="0" align="center"> 
 <tbody> 
@@ -15,7 +15,7 @@
 <h3><a href="product.html">Туфли Gucci</a></h3>
 <p class="s_model">Товар №2</p>
 <p class="s_price s_promo_price"><span class="s_old_price">
-<span class="s_currency s_before">$</span><strike>100</strike></span><span class="s_currency s_before">$</span>80</p>
+<span class="s_currency s_before">$</span><strike>250</strike></span><span class="s_currency s_before">$</span>220</p>
 <a class="" href=".html">Добавить в корзину</a>
       
 </td></tr>
@@ -25,45 +25,16 @@
 <h3><a href="product.html">Leica M7</a></h3>
 <p class="s_model">Товар №3</p>
 <p class="s_price s_promo_price"><span class="s_old_price">
-<span class="s_currency s_before">$</span><strike>100</strike></span><span class="s_currency s_before">$</span>80</p>
+<span class="s_currency s_before">$</span><strike>150</strike></span><span class="s_currency s_before">$</span>110</p>
 <a class="" href=".html">Добавить в корзину</a>
       
 </td><td style="border-image: initial;" width="40" height=""></td><td style="border-image: initial;"> 
 <p align="center"><img src="images/dummy/pic_4.jpg" border="0" width="400" height="300"/></p>
-<h3><a href="product.html">Компактный фотоаппарат Panasonic Lumix DMC-FZ1000</a></h3>
+<h3><a href="product.html">Giorgio Armani Acqua di Gio pour homme</a></h3>
 <p class="s_model">Товар №4</p>
 <p class="s_price s_promo_price"><span class="s_old_price">
-<span class="s_currency s_before">$</span><strike>100</strike></span><span class="s_currency s_before">$</span>80</p>
+<span class="s_currency s_before">$</span><strike>110</strike></span><span class="s_currency s_before">$</span>90</p>
 <a class="" href=".html">Добавить в корзину</a>
 </td></tr>
 </tbody>
 </table>
-      
-      
-      
-      
-      <h2 class="s_title_1"><span class="s_main_color">Товары по акции</span></h2>
-      <div class="clear"></div>
-      <div class="s_listing s_grid_view clearfix">
-        <div class="s_item grid_2"><img src="images/dummy/pic_5.jpg" title="Armani Acqua di Gioia" alt="Armani Acqua di Gioia" /></a>
-          <h3><a href="product.html">Armani Acqua di Gioia</a></h3>
-          <p class="s_model">Товар №1</p>
-          <p class="s_price s_promo_price"><span class="s_old_price"><span class="s_currency s_before">$</span><strike>100</strike></span><span class="s_currency s_before">$</span>80</p>
-          <a class="s_button_add_to_cart" href="product.html"><span class="s_icon_16"><span class="s_icon"></span>Добавить в корзину</span></a>
-        </div>
-        <div class="s_item grid_2"> <a class="s_thumb" href="product.html"><img src="images/dummy/pic_6.jpg" title="Hennesy Paradis Extra" alt="Hennesy Paradis Extra" /></a>
-          <h3><a href="product.html">Hennesy Paradis Extra</a></h3>
-          <p class="s_model">Товар №2</p>
-          <p class="s_price s_promo_price"><span class="s_old_price"><span class="s_currency s_before">$</span><strike>300</strike></span><span class="s_currency s_before">$</span>250</p>
-          <a class="s_button_add_to_cart" href="product.html"><span class="s_icon_16"><span class="s_icon"></span>Добавить в корзину</span></a>
-        </div>
-        <div class="s_item grid_2"> <a class="s_thumb" href="product.html"><img src="images/dummy/pic_7.jpg" title="Leica M7" alt="Leica M7" /></a>
-          <h3><a href="product.html">Leica M7</a></h3>
-          <p class="s_model">Товар №3</p>
-          <p class="s_price s_promo_price"><span class="s_old_price"><span class="s_currency s_before">$</span><strike>150</strike></span><span class="s_currency s_before">$</span>100</p>
-          <a class="s_button_add_to_cart" href="product.html"><span class="s_icon_16"><span class="s_icon"></span>Добавить в корзину</span></a>
-        </div>
-       
-        <div class="clear"></div>
-      </div>
-    </div>
