eda32338dea1ec0b23ce1f89ec146c4b1c2dbb77
YanaGumbatova555 <31986459+YanaGumbatova555@users.noreply.github.com>
2018-03-31 22:55:05 +0300

    Update closes3.php

--- closes3.php
+++ closes3.php
@@ -2,16 +2,16 @@
 <table width="996" height="711" cellspacing="1" cellpadding="1" border="0" align="center"> 
 <tbody> 
 <tr><td style="border-image: initial; width="40" height="" align="center"> 
-<p align="center"><img src="images/dummy/pic_5.jpg" border="0" width="400" height="300"/></p>
-<h3><a href="product.html">Armani Acqua di Gioia</a></h3>
+<p align="center"><img src="closes/childrencloses1.jpg" border="0" width="400" height="300"/></p>
+<h3><a href="product.html">Платье</a></h3>
 <p class="s_model">Товар №1</p>
 <p class="s_price s_promo_price"><span class="s_old_price">
 <span class="s_currency s_before">$</span><strike>100</strike></span><span class="s_currency s_before">$</span>80</p>
 <a class="" href=".html">Добавить в корзину</a>
       
 </td><td style="border-image: initial;" width="40" height="" align="center"></td><td style="border-image: initial;" align="center"> 
-<p align="center"><img src="images/dummy/pic_6.jpg" border="0" width="400" height="300"/></p>
-<h3><a href="product.html">Туфли Gucci</a></h3>
+<p align="center"><img src="closes/childrencloses2.jpg" border="0" width="400" height="300"/></p>
+<h3><a href="product.html">Спортивный костюм для мальчика</a></h3>
 <p class="s_model">Товар №2</p>
 <p class="s_price s_promo_price"><span class="s_old_price">
 <span class="s_currency s_before">$</span><strike>250</strike></span><span class="s_currency s_before">$</span>220</p>
@@ -20,16 +20,16 @@
 </td></tr>
       
 <tr align="center"><td style="border-image: initial;"> 
-<p align="center"><img src="images/dummy/pic_7.jpg" border="0" width="400" height="300"/></p>
-<h3><a href="product.html">Leica M7</a></h3>
+<p align="center"><img src="closes/childrencloses3.jpg" border="0" width="400" height="300"/></p>
+<h3><a href="product.html">Куртка для мальчика</a></h3>
 <p class="s_model">Товар №3</p>
 <p class="s_price s_promo_price"><span class="s_old_price">
 <span class="s_currency s_before">$</span><strike>150</strike></span><span class="s_currency s_before">$</span>110</p>
 <a class="" href=".html">Добавить в корзину</a>
       
 </td><td style="border-image: initial;" width="40" height=""></td><td style="border-image: initial;"> 
-<p align="center"><img src="images/dummy/pic_4.jpg" border="0" width="400" height="300"/></p>
-<h3><a href="product.html">Giorgio Armani Acqua di Gio pour homme</a></h3>
+<p align="center"><img src="closes/childrencloses4.jpg" border="0" width="400" height="300"/></p>
+<h3><a href="product.html">Куртка для девочки</a></h3>
 <p class="s_model">Товар №4</p>
 <p class="s_price s_promo_price"><span class="s_old_price">
 <span class="s_currency s_before">$</span><strike>110</strike></span><span class="s_currency s_before">$</span>90</p>
