6fb77693d07f16985a0d438222658c67744a6c67
YanaGumbatova555 <31986459+YanaGumbatova555@users.noreply.github.com>
2018-04-01 00:01:27 +0300

    Update connection.php

--- connection.php
+++ connection.php
@@ -1,1 +1,11 @@
+<?php
 
+$host = "sqlsrv:server = tcp:servgumb.database.windows.net,1433";
+$user = "Yana";
+$password = "Sobachka.1";
+$db = "db1";
+
+$connection = mysqli_connection($host, $user, $password) or die("Ошибка подключения к Серверу!");
+$select_bd = mysqli_select_db($connection, $db) or die("Ошибка подключения к Базе данных!");
+
+?>
