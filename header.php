a27fe3900ec3219f5c7f56deeb461749dbd55bb7
YanaGumbatova555 <31986459+YanaGumbatova555@users.noreply.github.com>
2018-03-31 23:15:27 +0300

    Update header.php

--- header.php
+++ header.php
@@ -9,7 +9,7 @@
 
 <form action="login.php" method="get">
 <label>Логин:</label><br>
-<input type="text" name="name" placeholder="Введите ваш логин">
+<input type="text" name="name" placeholder="Введите ваш логин"><br>
 <label>Пароль:</label><br>
 <input type="password" name="name" placeholder="1234567890"><br>
 <input type="submit" name="submit" value="Войти"><br>
