449d49380ea16e180872fabadc4edcd03cb0d21a
YanaGumbatova555 <31986459+YanaGumbatova555@users.noreply.github.com>
2018-04-01 17:45:49 +0300

    Create index1.php

---
+++ index1.php
@@ -0,0 +1,62 @@
+<!DOCTYPE html>
+<html>
+<head>
+<link  href="/reset.css" rel="stylesheet" type="text/css"/>  
+<link  href="/style.css" rel="stylesheet" type="text/css"/>  
+<title>Интернет-магазин</title> 
+
+</head>
+
+<body>
+    
+<div id="blockbody">  
+    
+           <div id="block-cart">    
+               <p>Моя корзина (<span class="count">0</span>)<span id="corner" class="corner-down"></span></p>
+                   <div>
+                       <p>Товаров: <span class="count">0</span></p>
+                       <p>На сумму: <span price="0" id="price">0</span></p>
+                       <a href="">Оформить</a>
+           </div> 
+   </div> 
+    <div id="block-content">   
+    
+        <p class="messagecart"></p>
+        
+        <ul id="listing">
+            <li>
+                <div class="fixblock">
+                    <center><img src=/closes/womencloses1.png" /></center>
+                </div>
+              <p class="price-tovar">2 500 руб</p>
+             <p price="2500" rel="1" class="add-tovar"></p>
+           </li>
+             
+          <li>
+                <div class="fixblock">
+                    <center><img src=/closes/womencloses2.png" /></center>
+                </div>
+              <p class="price-tovar">2 500 руб</p>
+             <p price="2500" rel="1" class="add-tovar"></p>
+        </li>      
+            
+       <li>
+                <div class="fixblock">
+                    <center><img src=/closes/womencloses3.png" /></center>
+                </div>
+              <p class="price-tovar">2 500 руб</p>
+             <p price="2500" rel="1" class="add-tovar"></p>
+           </li>
+                        
+        <li>
+                <div class="fixblock">
+                    <center><img src=/closes/womencloses4.png" /></center>
+                </div>
+              <p class="price-tovar">2 500 руб</p>
+             <p price="2500" rel="1" class="add-tovar"></p>
+           </li>
+  </div>
+ 
+</body>
+  
+</html>
