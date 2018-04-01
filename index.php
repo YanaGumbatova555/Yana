93892196b8f760cecec2123d64cf342c611ad5ef
YanaGumbatova555 <31986459+YanaGumbatova555@users.noreply.github.com>
2018-04-01 15:46:11 +0300

    Update index.php

--- index.php
+++ index.php
@@ -1,9 +1,8 @@
 <!DOCTYPE html>
 <html>
-      <?php include_once "header.php" ?>
-<?php include_once "leftblock.php" ?>
+<?php include_once "header.php" ?>
 <head>
-
+<?php include_once "leftblock.php" ?>
 
 <link  href="/style.css" rel="stylesheet" type="text/css"/>
 <title>Интернет-магазин</title> 
