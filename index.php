f614d14ee412fb847412774eb1ac2816d3e677f7
YanaGumbatova555 <31986459+YanaGumbatova555@users.noreply.github.com>
2018-04-01 16:26:01 +0300

    Update index.php

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 <head>
 <?php include_once "header.php" ?>
 <?php include_once "leftblock.php" ?>
-    <?php include_once "footer.php" ?> 
+<?php include_once "footer.php" ?> 
 <link  href="/style.css" rel="stylesheet" type="text/css"/>  
 <title>Интернет-магазин</title> 
 
@@ -14,7 +14,7 @@
 <div id="blockbody">  
     
            <div id="block-cart">    
-               <p>Моя корзина (<span class="count">0</span>)<span id="corner" class="corner-down"></span>
+               <p>Моя корзина (<span class="count">0</span>)<span id="corner" class="corner-down"></span></p>
                    <div>
                        <p>Товаров: <span class="count">0</span></p>
                        <p>На сумму: <span price="0" id="price">0</span></p>
