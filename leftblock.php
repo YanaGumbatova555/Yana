ff9bb02cb7591b51be193c65096469c8026ab521
YanaGumbatova555 <31986459+YanaGumbatova555@users.noreply.github.com>
2018-04-01 15:19:18 +0300

    Update leftblock.php

--- leftblock.php
+++ leftblock.php
@@ -7,8 +7,8 @@
 </ul>
   
   <div class="content">
-  <p align="center"><img src="/баннер1.png" align="centre" border="0" width="400" height="200"/></p>
-  <p align="center"><img src="/баннер2.png" align="centre" border="0" width="400" height="200"/></p>
+  <p align="center"><img src="/баннер1.png" border="0" width="200" height="100"/></p>
+  <p align="center"><img src="/баннер2.png" border="0" width="200" height="100"/></p>
    
   </div>
 </div>
