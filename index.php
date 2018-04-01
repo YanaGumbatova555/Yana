b7848c72342a0343a35d724390394a815ab1397a
YanaGumbatova555 <31986459+YanaGumbatova555@users.noreply.github.com>
2018-04-01 16:01:06 +0300

    Update index.php

--- index.php
+++ index.php
@@ -11,11 +11,16 @@
 <body>
     
 <div id="blockbody">  
-    <div id="block-content">    
+    
            <div id="block-cart">    
- 
+               <p>Моя корзина (<span class="count">0</span>)<span id="corner" class="corner-down"></span>
+                   <div>
+                       <p>Товаров: <span class="count">0</span></p>
+                       <p>На сумму: <span price="0" id="price">0</span></p>
+                       <a href="">Оормить</a>
            </div> 
     </div> 
+    <div id="block-content">    
 </div>
     <?php include_once "footer.php" ?> 
 </body>
