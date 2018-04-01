b0c59afe9bf8ca6d4524b56879066d2129a2e49f
YanaGumbatova555 <31986459+YanaGumbatova555@users.noreply.github.com>
2018-04-01 18:58:15 +0300

    Update leftblock.php

--- leftblock.php
+++ leftblock.php
@@ -3,7 +3,16 @@
   <div class="content">
   <p align="center"><img src="/баннер1.png" border="0" width="200" height="100"/></p>
   <p align="center"><img src="/баннер2.png" border="0" width="200" height="100"/></p>
-   
+   <div class="loginform">   
+<form action="login.php" method="get">
+<label>Логин:</label><br>
+<input type="text" name="name" placeholder="Введите ваш логин"><br>
+<label>Пароль:</label><br>
+<input type="password" name="password" placeholder="1234567890"><br>
+<input type="submit" name="submit" value="Войти">
+<button formaction="registration.php">Регистрация</button>
+  </form>      
+      </div>
   </div>
 </div>
 
