3a715969273acc815e0045104a0716f2d879f9ce
YanaGumbatova555 <31986459+YanaGumbatova555@users.noreply.github.com>
2018-04-01 18:59:39 +0300

    Update leftblock.php

--- leftblock.php
+++ leftblock.php
@@ -1,8 +1,5 @@
 <div class="leftblock"> 
-  
-  <div class="content">
-  <p align="center"><img src="/баннер1.png" border="0" width="200" height="100"/></p>
-  <p align="center"><img src="/баннер2.png" border="0" width="200" height="100"/></p>
+<div class="content">
    <div class="loginform">   
 <form action="login.php" method="get">
 <label>Логин:</label><br>
