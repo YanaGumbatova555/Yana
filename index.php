f12d9cd8579cf28c1de5511814c54423eb14305a
YanaGumbatova555 <31986459+YanaGumbatova555@users.noreply.github.com>
2018-04-01 15:32:44 +0300

    Update index.php

--- index.php
+++ index.php
@@ -1,9 +1,20 @@
+<!DOCTYPE html>
 <html>
 <head>
+<link  href="/style.css" rel="stylesheet" type="text/css"/>
 <title>Интернет-магазин</title>
 <?php include_once "header.php" ?>
-  <?php include_once "leftblock.php" ?>
-  
+<?php include_once "leftblock.php" ?>
+<?php include_once "footer.php" ?>  
+
+</head>
+
+<body>
     
-  <?php include_once "footer.php" ?>
+<div id="blockbody">  
+</div>
+    
+</body>
+  
 </html>
+
