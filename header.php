202a92eba72b08c0186fb21ab2ba941643c792e9
YanaGumbatova555 <31986459+YanaGumbatova555@users.noreply.github.com>
2018-04-01 19:00:27 +0300

    Update header.php

--- header.php
+++ header.php
@@ -4,16 +4,5 @@
 </head>
 
 <div class="header"> 
-  
-<div class="loginform">   
-
-<form action="login.php" method="get">
-<label>Логин:</label><br>
-<input type="text" name="name" placeholder="Введите ваш логин"><br>
-<label>Пароль:</label><br>
-<input type="password" name="password" placeholder="1234567890"><br>
-<input type="submit" name="submit" value="Войти">
-<button formaction="registration.php">Регистрация</button>
-  </form>      
-      </div>
+  <p>Магазин одежды</p>
     </div>
