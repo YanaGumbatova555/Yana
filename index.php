e75398c6fee483a3ba92b8aa7d523e99b6b4cd8c
YanaGumbatova555 <31986459+YanaGumbatova555@users.noreply.github.com>
2018-04-01 15:07:40 +0300

    Update index.php

--- index.php
+++ index.php
@@ -4,10 +4,6 @@
 <?php include_once "header.php" ?>
   <?php include_once "leftblock.php" ?>
   
-  <div class="content">
-  <p align="center"><img src="/баннер1.png" border="0" width="500" height="300"/></p>
-  <p align="center"><img src="/баннер2.png" border="0" width="500" height="300"/></p>
-    </div>
     
   <?php include_once "footer.php" ?>
 </html>
