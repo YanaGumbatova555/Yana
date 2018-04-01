132852baad4dcd2ade20c68d7b2c7d6afe43d7fb
YanaGumbatova555 <31986459+YanaGumbatova555@users.noreply.github.com>
2018-04-01 15:07:30 +0300

    Update leftblock.php

--- leftblock.php
+++ leftblock.php
@@ -5,5 +5,11 @@
   <li><a href="\closes2.php"> Мужская одежда</a></li>
   <li><a href="\closes3.php"> Детская одежда</a></li>
 </ul>
+  
+  <div class="content">
+  <p align="center"><img src="/баннер1.png" border="0" width="100" height="50"/></p>
+  <p align="center"><img src="/баннер2.png" border="0" width="100" height="50"/></p>
+    
+  </div>
 </div>
 
