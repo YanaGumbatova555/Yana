4dd50acee44638c84e6a3404e12aedb150115349
YanaGumbatova555 <31986459+YanaGumbatova555@users.noreply.github.com>
2018-04-05 20:52:52 +0300

    Update sugnup.php

--- sugnup.php
+++ sugnup.php
@@ -73,12 +73,12 @@ try
 {
   $conn = new PDO("sqlsrv:server = tcp:servgumb.database.windows.net,1433; Database = db1", "Yana", "Sobachka.1");
   $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-}
-catch (PDOException $e) 
-{
-print("Error connecting to SQL Server.");
-die(print_r($e));
-}
+} 
+catch (PDOException $e) { 
+print("Ошибка подключения к SQL Server."); 
+die(print_r($e)); 
+} 
+
 
 if (isset($_POST['submit'])) {
 $lastname = $_POST['lastname'];
@@ -89,29 +89,16 @@ $login = $_POST['login'];
 $password = $_POST['password'];
 $password2 = $_POST['password2'];
   
-$err = array(); 
-if(empty($lastname)) {
-$err[] = 'Поле "Фамилия" не заполненно!';
-}
-elseif(empty($name)) {
-$err[] = 'Поле Имя не заполненно!';
-}
-elseif(empty($middlename)) {
-$err[] = 'Поле Отчество не заполненно!';
-}  
-elseif(empty($Email)) {
-$err[] = 'Поле E-mail не заполненно!';
-}
-  elseif(empty($login)) {
-$err[] = 'Поле Логин не заполненно!';
-}
-elseif(empty($password)) {
-$err[] = 'Поле пароль не заполненно!';
-}
-elseif($password != $password2) {
-$err[] = 'Неправельно заполнен пароль!';
-}
-
+if(!empty($_POST)) { 
+try { 
+$lastname = $_POST['lastname']; 
+$name = $_POST['name']; 
+$middlename = $_POST['middlename']; 
+$Email = $_POST['Email']; 
+$login = $_POST['login']; 
+$password = $_POST['password']; 
+$password2 = $_POST['password2'];  
+  
 $sql_insert = "INSERT INTO table1 (lastname, name, middlename, Email, login, password, password2) VALUES (?,?,?,?,?,?,?)";
 $stmt = $conn->prepare($sql_insert);
 $stmt->bindValue(1, $lastname);
@@ -122,13 +109,19 @@ $stmt->bindValue(4, $Email);
 $stmt->bindValue(6, $password);
 $stmt->bindValue(7, $password2);
 $stmt->execute();
-echo '<div style= "color: blue; text-align: center;">Вы зарегистрированны!</div><hr>';
+}
+  catch(Exception $e) { 
+die(var_dump($e)); 
+} 
+echo "<h3>Вы зарегистрированы!</h3>"; 
+} 
 
 $sql_select = "SELECT * FROM table1";
 $stmt = $conn->query($sql_select);
 $registrants = $stmt->fetchAll();
 if(count($registrants) > 0) 
 {
+echo "<h2>Зарегестрированные пользователи:</h2>"; 
 echo "<table>";
 echo "<tr><th>lastname-</th></br>";
 echo "<th>name-</th></br>";
@@ -144,6 +137,10 @@ echo "<td>".$registrant['middlename']."</td>";
 echo "<td>".$registrant['Email']."</td>";
   echo "<td>".$registrant['login']."</td></tr>";
 }
-echo "</table>";
-}
+echo "</table>"; 
+} else 
+{ 
+echo "<h3>Вы не зарегистрированы!</h3>"; 
+} 
+
 ?> 
