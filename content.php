a1f773e33ff48f0156c4a1dfc80a21c4f803c9fe
YanaGumbatova555 <31986459+YanaGumbatova555@users.noreply.github.com>
2018-04-05 23:12:57 +0300

    Create content.php

---
+++ content.php
@@ -0,0 +1,9 @@
+<html>
+<head>
+<?php include_once "header.php" ?>
+<?php include_once "leftblock.php" ?>
+<?php include_once "footer.php" ?> 
+ 
+<title>Интернет-магазин</title> 
+</head>  
+</html>
