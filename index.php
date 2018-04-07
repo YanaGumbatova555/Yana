dae26b53887c9f8d04e8bb11fd01946c4264b3ca
YanaGumbatova555 <31986459+YanaGumbatova555@users.noreply.github.com>
2018-04-07 12:59:20 +0300

    Update index.php

--- index.php
+++ index.php
@@ -1,1 +1,47 @@
+<?php
+require_once('db1.php');
+  if (isset($_POST['submit'])) {
+    $user_username = $_POST['username'];
+    $user_password = $_POST['password'];
+    if (!empty($user_username) && !empty($user_password)) {
+      $_SESSION["username"] = $user_username;
+      $sql_select = "SELECT 'id', 'username' FROM signup WHERE username = '$user_username' AND password = '$user_password'";
+      $stmt = $conn->query($sql_select);
+      $stmt->execute();
+      $data = $stmt->fetchAll();
+    }
+    else {
+      echo 'Поля заполнены неправельно';
+    }
+  }
+ ?>
 
+
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <link rel="stylesheet" href="/css/style1.css">
+    <title>Авторизация</title>
+  </head>
+  <body>
+    <div class="container">
+ 
+
+      <img src="img/lock.png">
+      <form class="" action="index.php" method="post">
+        <div class="dws-input">
+          <input type="text" name="username" placeholder="Введите логин">
+        </div>
+        <div class="dws-input">
+          <input type="password" name="password" placeholder="Введите пароль">
+        </div>
+        <input class="dws-submit" type="submit" name="submit" value="Войти">
+        <br>
+        <a href="\sugnup.php">Регистрация</a>
+      </form>
+
+
+    </div>
+  </body>
+</html>
