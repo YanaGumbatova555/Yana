9e1d10f93490cb1f44c4df3dee8adedc37838251
YanaGumbatova555 <31986459+YanaGumbatova555@users.noreply.github.com>
2018-04-07 14:40:30 +0300

    Update centre.php

--- centre.php
+++ centre.php
@@ -1,9 +1,12 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <link href="style.css" rel="stylesheet">
-   <div class="footer">
+<link href="style.css" rel="stylesheet">
+
+    <div class="centre">
 @YanaGumbatova
 </div>
+    
+</head>    
 </html>
-</head>
+
