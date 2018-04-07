55dc01e499c68dbfb884d1aad64373a390c5ff83
YanaGumbatova555 <31986459+YanaGumbatova555@users.noreply.github.com>
2018-04-07 14:11:52 +0300

    Update header.php

--- header.php
+++ header.php
@@ -6,9 +6,11 @@
     
 <body>
 <div class="header"> 
+  <a href="/index.php">
 <ul>
-  <li><a href="\index.php"> АКЦИЯ</a></li>
-  <li><a href="\sugnup.php"> Женская одежда</a></li>
+  <li><a href="\index.php"> Вход</a></li>
+  <li><a href="\sugnup.php"> Регистрация</a></li>
+  <input class="dws-submit" type="submit" name="submit" value="Вход">
 </ul>
 </div>
 </body>
