10baa083ddbd66abadf3e7ab57e2230250ac523f
YanaGumbatova555 <31986459+YanaGumbatova555@users.noreply.github.com>
2018-04-07 14:01:14 +0300

    Update header.php

--- header.php
+++ header.php
@@ -1,8 +1,12 @@
-<head>
-<link href="/style.css" rel="stylesheet">
+<!DOCTYPE html>
+	<html>
+  <head>
+<link rel="stylesheet" href="/style.css">
 </head>
-
+    
+<body>
 <div class="header"> 
 <button formaction="index.php">Вход</button>
 <button formaction="sugnup.php">Регистрация</button>
 </div>
+    </body>
