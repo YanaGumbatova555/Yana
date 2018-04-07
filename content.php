0287e80be18a64a4186cfafdf54a665f146c8742
YanaGumbatova555 <31986459+YanaGumbatova555@users.noreply.github.com>
2018-04-07 14:45:39 +0300

    Update content.php

--- content.php
+++ content.php
@@ -2,6 +2,7 @@
 <head>
 <link href="style.css" rel="stylesheet">
 <?php include_once "header.php" ?>
+ <?php include_once "centre.php" ?>
 <?php include_once "footer.php" ?> 
  
 <title>Интернет-магазин</title> 
