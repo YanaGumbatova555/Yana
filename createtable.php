c02ff2a743d34a9e242bfee8a39bf2239d2bd95a
YanaGumbatova555 <31986459+YanaGumbatova555@users.noreply.github.com>
2018-04-07 16:13:52 +0300

    Update createtable.php

--- createtable.php
+++ createtable.php
@@ -7,16 +7,15 @@ try {
     $conn = new PDO($dsn, $login, $pass);
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-$sql = "CREATE TABLE table1(
+$sql = "CREATE TABLE t1(
     id INT NOT NULL IDENTITY(1,1) 
     PRIMARY KEY(id),
     lastname VARCHAR(30),
     name VARCHAR(30),
     middlename VARCHAR(30),
-    Email VARCHAR(30),
+    email VARCHAR(30),
     login VARCHAR(30),
-    password VARCHAR(30),
-    password2 VARCHAR(30)";
+    password VARCHAR(30)";
     $conn->query($sql);
     
     echo "<h3>Таблица создана!</h3>"; 
