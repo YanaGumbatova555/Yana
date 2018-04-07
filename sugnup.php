9bfd8bcafc3628ba173a865beccdc2c06677ee15
YanaGumbatova555 <31986459+YanaGumbatova555@users.noreply.github.com>
2018-04-07 13:09:57 +0300

    Update sugnup.php

--- sugnup.php
+++ sugnup.php
@@ -7,38 +7,19 @@
 	  </head>
 	  <body>
 	    <div class="container">
-		    <a href="/index.php"></a>
+		    <a href="/index.php"><img src="/fon.png"></a>
 	            <form class="" action="sugnup.php" method="post">
 	              <legend><h1>Регистрация</h1></legend>
-	              <div class="form-group">
-	                <label for="" >Фамилия</label>
-	                <input class="form-control" type="text" name="lastname" value="">
-	              </div>
-	              <div class="form-group">
-	                <label for="">Имя</label>
-	                <input class="form-control" type="text" name="name" value="">
-	              </div>
-	              <div class="form-group">
-	                <label for="">Отчество</label>
-	                <input class="form-control" type="text" name="middlename">
-	              </div>
-	              <div class="form-group">
-	                <label for="">Email</label>
-	                <input class="form-control" type="text" name="Email">
-	              </div>
-	              <div class="form-group">
-	                <label for="">Логин</label>
-	                <input class="form-control" type="text" name="login">
-	              </div>
-	              <div class="form-group">
-	                <label for="">Пароль</label>
-	                <input class="form-control" type="password" name="password">
-	              </div>
-	              <div class="form-group">
-	                <label>Повторите пароль</label>
-	                <input class="form-control" type="password" name="password2">
-	              </div>
-	              <input class="sub" type="submit" name="submit" value="Регистрация">
+	              <div class="dws-input">
+<input type="text" name="lastname" placeholder="Введите фамилию...">
+<input type="text" name="name" placeholder="Введите имя...">
+<input type="text" name="middlename" placeholder="Введите отчество...">
+<input type="text" name="Email" placeholder="Введите ваш адрес эл.почты...">
+<input type="text" name="login" placeholder="Придумайте логин...">
+<input type="password" name="password" placeholder="Придумайте пароль...">
+<input type="password" name="password2" placeholder="Повторите пароль...">
+			    </div>
+<input class="sub" type="submit" name="submit" value="Регистрация">
 	            </form>
 	        </div>
 	  </body>
@@ -49,8 +30,8 @@
 
 	require_once('db1.php');
 	
-	if (isset($_POST['submit'])) 
-	{
+	if (isset($_POST['submit'])) {
+		
 	$lastname = $_POST['lastname'];
 	$name = $_POST['name'];
 	$middlename = $_POST['middlename'];
@@ -64,19 +45,19 @@
 	$err[] = 'Поле "Фамилия" не заполненно!';
 	}
 	elseif($name = '') {
-	$err[] = 'Поле Имя не заполненно!';
+	$err[] = 'Поле "Имя" не заполненно!';
 	}
 	elseif($middlename = '') {
-	$err[] = 'Поле Отчество не заполненно!';
+	$err[] = 'Поле "Отчество" не заполненно!';
 	}  
 	elseif($Email = '') {
-	$err[] = 'Поле E-mail не заполненно!';
+	$err[] = 'Поле "E-mail" не заполненно!';
 	}
 	  elseif($login = '') {
-	$err[] = 'Поле Логин не заполненно!';
+	$err[] = 'Поле "Логин" не заполненно!';
 	}
 	elseif($password = '') {
-	$err[] = 'Поле пароль не заполненно!';
+	$err[] = 'Поле "Пароль" не заполненно!';
 	}
 	elseif($password != $password2) {
 	$err[] = 'Не верно заполнен пароль!';
@@ -84,14 +65,14 @@
 	
 	
 	    if(empty($err)) {
-        $sql_select = "SELECT * FROM signup WHERE username = '$lastname'";
+        $sql_select = "SELECT * FROM table1 WHERE username = '$lastname'";
         $stmt = $conn->query($sql_select);
         $stmt->execute();
         $data = $stmt->fetchAll();
 		
 		    
 		    if(count($data) == 0) {
-            $sql_insert = "INSERT INTO table1 (lastname, name, middlename, Email, login, password, password2) VALUES (?,?,?,?,?,?,?)";
+        $sql_insert = "INSERT INTO table1 (lastname, name, middlename, Email, login, password, password2) VALUES (?,?,?,?,?,?,?)";
 	$stmt = $conn->prepare($sql_insert);
 	$stmt->bindValue(1, $lastname);
 	$stmt->bindValue(2, $name);
@@ -102,7 +83,7 @@
 	$stmt->bindValue(7, $password2);
 	$stmt->execute();
             
-            echo '<div style= "color: white;">Вы зарегистрированны!</div><hr>';
+            echo '<div style= "color: black;">Вы зарегистрированны!</div><hr>';
         }
         else {
             echo '<div style = "color: red;">Такой пользователь уже существует!</div><hr>';
