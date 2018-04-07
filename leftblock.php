18286fa500693f22f2c9daca55636d59eba73f27
YanaGumbatova555 <31986459+YanaGumbatova555@users.noreply.github.com>
2018-04-07 13:53:12 +0300

    Update leftblock.php

--- leftblock.php
+++ leftblock.php
@@ -1,14 +1,6 @@
 <div class="leftblock"> 
 <div class="content">
-<div class="loginform">   
-<form action="login.php" method="get">
-<label>Логин:</label><br>
-<input type="text" name="name" placeholder="Введите ваш логин"><br>
-<label>Пароль:</label><br>
-<input type="password" name="password" placeholder="1234567890"><br>
-<input type="submit" name="submit" value="Войти">
-<button formaction="sugnup.php">Регистрация</button>
-</form>      
+<div class="loginform">        
 </div>
 </div>
 </div>
